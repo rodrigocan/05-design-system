23fb698e41a0b82719c8ae542fd6fa519f0111d1
Manoel Rodrigo Cândido <rodrigocan@gmail.com>
2023-01-24 15:12:38 -0300

    Componente: Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis reiciendis cumque, expedita blanditiis molestias dolore numquam quod et velit nostrum. Alias, necessitatibus blanditiis ipsum quia nostrum molestiae illum reiciendis optio.',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
